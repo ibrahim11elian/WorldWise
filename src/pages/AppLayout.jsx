6dd5e17d55d6a822b3c6391862af52db2bf5e847
ibrahim11elian <ibrahim11elian@gmail.com>
2024-07-04 13:33:38 +0300

    change nav to main tag

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -4,11 +4,11 @@ import User from "../components/User";
 import styles from "./AppLayout.module.css";
 function AppLayout() {
   return (
-    <nav className={styles.app}>
+    <main className={styles.app}>
       <Sidebar />
       <Map />
       <User />
-    </nav>
+    </main>
   );
 }
 
